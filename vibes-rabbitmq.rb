b7795a53541c2f93540e79c27bbc2ea4ddbe1701
Matt Campbell <matt@soupmatt.com>
2013-12-12 14:17:57 -0600

    add rabbitmq formula

---
+++ vibes-rabbitmq.rb
@@ -0,0 +1,99 @@
+require 'formula'
+
+class VibesRabbitmq < Formula
+  homepage 'http://www.rabbitmq.com'
+  url 'http://www.rabbitmq.com/releases/rabbitmq-server/v3.1.5/rabbitmq-server-mac-standalone-3.1.5.tar.gz'
+  sha1 'f7f112e7278b5fa449104358da61d937fa7e7936'
+
+  depends_on 'simplejson' => :python if MacOS.version <= :leopard
+
+  conflicts_with 'rabbitmq',
+    :because => 'they are really the same thing'
+
+  def install
+    # Install the base files
+    prefix.install Dir['*']
+
+    # Setup the lib files
+    (var+'lib/rabbitmq').mkpath
+    (var+'log/rabbitmq').mkpath
+
+    # Correct SYS_PREFIX for things like rabbitmq-plugins
+    inreplace sbin/'rabbitmq-defaults' do |s|
+      s.gsub! 'SYS_PREFIX=${RABBITMQ_HOME}', "SYS_PREFIX=#{HOMEBREW_PREFIX}"
+      s.gsub! 'CLEAN_BOOT_FILE="${SYS_PREFIX}', "CLEAN_BOOT_FILE=\"#{prefix}"
+      s.gsub! 'SASL_BOOT_FILE="${SYS_PREFIX}', "SASL_BOOT_FILE=\"#{prefix}"
+    end
+
+    # Set RABBITMQ_HOME in rabbitmq-env
+    inreplace (sbin + 'rabbitmq-env'), 'RABBITMQ_HOME="${SCRIPT_DIR}/.."', "RABBITMQ_HOME=#{prefix}"
+
+    # Create the rabbitmq-env.conf file
+    rabbitmq_env_conf = etc+'rabbitmq/rabbitmq-env.conf'
+    rabbitmq_env_conf.write rabbitmq_env unless rabbitmq_env_conf.exist?
+
+    # Enable plugins - management web UI and STOMP protocol
+    enabled_plugins_path = etc+'rabbitmq/enabled_plugins'
+    enabled_plugins_path.write '[rabbitmq_management,rabbitmq_stomp].' unless enabled_plugins_path.exist?
+
+    # Extract rabbitmqadmin and install to sbin
+    # use it to generate, then install the bash completion file
+    system "/usr/bin/unzip", "-qq", "-j",
+           "#{prefix}/plugins/rabbitmq_management-#{version}.ez",
+           "rabbitmq_management-#{version}/priv/www/cli/rabbitmqadmin"
+
+    sbin.install 'rabbitmqadmin'
+    (sbin/'rabbitmqadmin').chmod 0755
+    (bash_completion/'rabbitmqadmin.bash').write `#{sbin}/rabbitmqadmin --bash-completion`
+  end
+
+  def post_install
+    %w(rabbitmq_management rabbitmq_federation rabbitmq_federation_management rabbitmq_stomp rabbitmq_web_stomp).each do |plugin|
+      ensure_plugin_installed(plugin)
+    end
+  end
+
+  def ensure_plugin_installed(plugin)
+    system "#{sbin}/rabbitmq-plugins enable #{plugin}"
+  end
+
+  def caveats; <<-EOS.undent
+    Management Plugin enabled by default at http://localhost:15672
+    EOS
+  end
+
+  def rabbitmq_env; <<-EOS.undent
+    CONFIG_FILE=#{etc}/rabbitmq/rabbitmq
+    NODE_IP_ADDRESS=127.0.0.1
+    NODENAME=rabbit@localhost
+    EOS
+  end
+
+  plist_options :manual => 'rabbitmq-server'
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN"
+    "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>Program</key>
+        <string>#{opt_prefix}/sbin/rabbitmq-server</string>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>EnvironmentVariables</key>
+        <dict>
+          <!-- need erl in the path -->
+          <key>PATH</key>
+          <string>/usr/local/sbin:/usr/bin:/bin:/usr/local/bin</string>
+          <!-- specify the path to the rabbitmq-env.conf file -->
+          <key>CONF_ENV_FILE</key>
+          <string>#{etc}/rabbitmq/rabbitmq-env.conf</string>
+        </dict>
+      </dict>
+    </plist>
+    EOS
+  end
+end
